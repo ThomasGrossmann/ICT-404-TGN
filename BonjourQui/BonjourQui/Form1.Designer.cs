5a24e1d6c9a08c18ccd96e14a2fcb93faa0da71f
ThomasGrossmann <47849503+ThomasGrossmann@users.noreply.github.com>
2019-05-06 14:33:27 +0200

    Étape1

---
+++ BonjourQui/BonjourQui/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace BonjourQui
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Name = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnBienvenue = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Name
+            // 
+            this.Name.AutoSize = true;
+            this.Name.Font = new System.Drawing.Font("Arial Rounded MT Bold", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.Name.Location = new System.Drawing.Point(25, 20);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(138, 29);
+            this.Name.TabIndex = 0;
+            this.Name.Text = "Votre nom";
+            this.Name.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(195, 20);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(224, 31);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnBienvenue
+            // 
+            this.btnBienvenue.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Maroon;
+            this.btnBienvenue.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBienvenue.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnBienvenue.Location = new System.Drawing.Point(150, 179);
+            this.btnBienvenue.Name = "btnBienvenue";
+            this.btnBienvenue.Size = new System.Drawing.Size(138, 49);
+            this.btnBienvenue.TabIndex = 2;
+            this.btnBienvenue.Text = "Bienvenue";
+            this.btnBienvenue.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 252);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBienvenue);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Name);
+            this.Name = "Form1";
+            this.Text = "Bonjour Qui ?";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Name;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnBienvenue;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
