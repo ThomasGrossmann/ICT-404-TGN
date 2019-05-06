d02239f939eb5206299745be8a8a2a5ef008a6fd
ThomasGrossmann <47849503+ThomasGrossmann@users.noreply.github.com>
2019-05-06 14:37:36 +0200

    Update Form1.Designer.cs

--- BonjourQui/BonjourQui/Form1.Designer.cs
+++ BonjourQui/BonjourQui/Form1.Designer.cs
@@ -29,9 +29,9 @@
         private void InitializeComponent()
         {
             this.Name = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.NameBox = new System.Windows.Forms.TextBox();
             this.btnBienvenue = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.BonjourNom = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // Name
@@ -45,14 +45,14 @@
             this.Name.Text = "Votre nom";
             this.Name.Click += new System.EventHandler(this.label1_Click);
             // 
-            // textBox1
+            // NameBox
             // 
-            this.textBox1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(195, 20);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(224, 31);
-            this.textBox1.TabIndex = 1;
+            this.NameBox.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameBox.Location = new System.Drawing.Point(195, 20);
+            this.NameBox.Multiline = true;
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(224, 31);
+            this.NameBox.TabIndex = 1;
             // 
             // btnBienvenue
             // 
@@ -66,24 +66,24 @@
             this.btnBienvenue.Text = "Bienvenue";
             this.btnBienvenue.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // BonjourNom
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(30, 110);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(37, 29);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "...";
+            this.BonjourNom.AutoSize = true;
+            this.BonjourNom.Font = new System.Drawing.Font("Arial Rounded MT Bold", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.BonjourNom.Location = new System.Drawing.Point(30, 110);
+            this.BonjourNom.Name = "BonjourNom";
+            this.BonjourNom.Size = new System.Drawing.Size(37, 29);
+            this.BonjourNom.TabIndex = 3;
+            this.BonjourNom.Text = "...";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(444, 252);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BonjourNom);
             this.Controls.Add(this.btnBienvenue);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.NameBox);
             this.Controls.Add(this.Name);
             this.Name = "Form1";
             this.Text = "Bonjour Qui ?";
@@ -96,9 +96,9 @@
         #endregion
 
         private System.Windows.Forms.Label Name;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox NameBox;
         private System.Windows.Forms.Button btnBienvenue;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label BonjourNom;
     }
 }
 
