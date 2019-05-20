96571589dd433a2da320acce42da7c21e7c5e415
ThomasGrossmann <47849503+ThomasGrossmann@users.noreply.github.com>
2019-05-20 15:57:22 +0200

    Exo Pizza WIP

---
+++ Pizza/Pizza/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Pizza
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTable = new System.Windows.Forms.Label();
+            this.txtTable = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblTable
+            // 
+            this.lblTable.AutoSize = true;
+            this.lblTable.Location = new System.Drawing.Point(42, 16);
+            this.lblTable.Name = "lblTable";
+            this.lblTable.Size = new System.Drawing.Size(34, 13);
+            this.lblTable.TabIndex = 0;
+            this.lblTable.Text = "Table";
+            // 
+            // txtTable
+            // 
+            this.txtTable.Location = new System.Drawing.Point(82, 13);
+            this.txtTable.Name = "txtTable";
+            this.txtTable.Size = new System.Drawing.Size(40, 20);
+            this.txtTable.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 307);
+            this.Controls.Add(this.txtTable);
+            this.Controls.Add(this.lblTable);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTable;
+        private System.Windows.Forms.TextBox txtTable;
+    }
+}
+
